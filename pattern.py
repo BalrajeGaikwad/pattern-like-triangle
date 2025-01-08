c433f697781d8024f891a0a3c53177b836dec39b
BalrajeGaikwad <balrajegaikwad55@gmail.com>
2025-01-08 19:50:18 +0530

    file added

---
+++ pattern.py
@@ -0,0 +1,21 @@
+"""
+
+     1
+    1 1
+   1 2 1
+  1 3 3 1
+ 1 4 6 4 1
+
+""" #print the psttern
+
+rows=5
+
+for i in range(rows):
+    print(" "*(rows-i),end="")
+
+    num=1
+    for j in range(i+1):
+        print(num, end=" ")
+        num=num*(i-j)//(j+1)
+
+    print()
